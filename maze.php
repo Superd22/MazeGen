42712320f8e84e36ce78a7d017053ebc4ee30699
Superd22 <superd001@gmail.com>
2014-12-06 15:16:36 +0100

    alékui

--- maze.php
+++ maze.php
@@ -8,16 +8,22 @@ error_reporting(E_ALL & ~E_NOTICE);
 		function __construct($width,$height) {
 				print_r("Génération d'un labyrinthe ".$width."x".$height."<br />");
 
+					// Génération des variables
 				$this->ouverts = array();
 				$this->width = $width;
 				$this->height = $height;
 				$this->size = $width * $height;
 
+					// Génération des cases
 				for($i=1;$i<=($width*$height);$i++) {
 					$this->cases[$i]['r'] = $this->cases[$i]['b'] = true;
+
+						// Fix pour les "bords" du labyrinthes
 					if( ($i % $this->width) == 0) {$this->cases[$i]['r'] = 0;}
 					if( $i/$this->width > $height-1) {$this->cases[$i]['b'] = 0;}
+					$this->ouverts[$i][] = $i;
 				}
+
 	    }
 
 	    private function drop_wall($n,$b) {
@@ -43,7 +49,10 @@ error_reporting(E_ALL & ~E_NOTICE);
 
 	    private function get_ouvert($n) {
 	    	$keyOuvert = false;
+
+	    			// On parcourt nos ouverts
 	    		foreach($this->ouverts as $key=>$ouvert) {
+	    				// A la recherche de la case.
 	    			if(in_array($n,$ouvert)) {
 	    				$keyOuvert = $key; 
 	    				break;
@@ -53,38 +62,39 @@ error_reporting(E_ALL & ~E_NOTICE);
 	    }
 
 	    private function rand_destroy() {
+
+	    		// Selection d'un mur
 	    	$n = rand(1,$this->size);
 	    	$mur = array_rand($this->cases[$n]);
+
 	    		if($this->cases[$n][$mur]) {
 				$voisin = $this->get_voisin($n,$mur);
+
+					// Si notre voisin existe
 				if($voisin) {
-						$ouvert_n = $this->get_ouvert($n);
-						$ouvert_voisin = $this->get_ouvert($voisin);
 
+						// On récupère les ouverts
+					$ouvert_n = $this->get_ouvert($n);
+					$ouvert_voisin = $this->get_ouvert($voisin);
 
+						// Si nos deux ouverts n'existent pas (= premiere initialisation) ou qu'ils sont différents
 					if( ($ouvert_n === FALSE AND $ouvert_voisin === FALSE) OR !($ouvert_n === $ouvert_voisin)) {
 						$this->drop_wall($n,$mur);
 
-						if($ouvert_n === false) {
-							$ouvert_n = sizeof($this->ouverts)+1;
-							$this->ouverts[$ouvert_n][] = $n;
-						}
-						if($ouvert_voisin === false) {
-							$ouvert_voisin = sizeof($this->ouverts)+1;
-							$this->ouverts[$ouvert_voisin][] = $voisin;
-						}
 
-							$this->ouverts[$ouvert_n] = array_merge($this->ouverts[$ouvert_voisin],$this->ouverts[$ouvert_n]);
-							unset($this->ouverts[$ouvert_voisin]);
-							$this->ouverts = array_values($this->ouverts);
-
+							// On merge les cases de l'ouvert du voisin avec l'ouvert n
+						$this->ouverts[$ouvert_n] = array_merge($this->ouverts[$ouvert_voisin],$this->ouverts[$ouvert_n]);
+							// On supprime l'ancien ouvert du voisin
+						unset($this->ouverts[$ouvert_voisin]);
+							// On réindexe nos ouverts.
+						//$this->ouverts = array_values($this->ouverts);
 					}
 				}
 			}
 	    }
 
 	    public function make_perfect() {
-	    	while(sizeof($this->ouverts[0]) < $this->size) {
+	    	while(sizeof($this->ouverts) > 1) {
 	    		$this->rand_destroy();
 	    	}
 
