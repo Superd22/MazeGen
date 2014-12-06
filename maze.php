79c9704b826e2fb12c542136b6c84990c66e6077
Superd22 <superd001@gmail.com>
2014-12-06 15:19:38 +0100

    ca

--- maze.php
+++ maze.php
@@ -21,7 +21,6 @@ error_reporting(E_ALL & ~E_NOTICE);
 						// Fix pour les "bords" du labyrinthes
 					if( ($i % $this->width) == 0) {$this->cases[$i]['r'] = 0;}
 					if( $i/$this->width > $height-1) {$this->cases[$i]['b'] = 0;}
-					$this->ouverts[$i][] = $i;
 				}
 
 	    }
@@ -81,20 +80,31 @@ error_reporting(E_ALL & ~E_NOTICE);
 					if( ($ouvert_n === FALSE AND $ouvert_voisin === FALSE) OR !($ouvert_n === $ouvert_voisin)) {
 						$this->drop_wall($n,$mur);
 
+							// Génération des ouverts pour la première fois si nécéssaire.
+						if($ouvert_n === false) {
+							$ouvert_n = sizeof($this->ouverts)+1;
+							$this->ouverts[$ouvert_n][] = $n;
+						}
+						if($ouvert_voisin === false) {
+							$ouvert_voisin = sizeof($this->ouverts)+1;
+							$this->ouverts[$ouvert_voisin][] = $voisin;
+						}
+
 
 							// On merge les cases de l'ouvert du voisin avec l'ouvert n
 						$this->ouverts[$ouvert_n] = array_merge($this->ouverts[$ouvert_voisin],$this->ouverts[$ouvert_n]);
 							// On supprime l'ancien ouvert du voisin
 						unset($this->ouverts[$ouvert_voisin]);
 							// On réindexe nos ouverts.
-						//$this->ouverts = array_values($this->ouverts);
+						$this->ouverts = array_values($this->ouverts);
 					}
 				}
 			}
 	    }
 
 	    public function make_perfect() {
-	    	while(sizeof($this->ouverts) > 1) {
+	    	while(sizeof($this->ouverts[0]) < $this->size) {
+	    		echo sizeof($this->ouverts). "<br />";
 	    		$this->rand_destroy();
 	    	}
 
